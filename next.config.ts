b92649aa33b2cb20fc0c76074956564f25c5bcda
Ayush Kumar <ayushkmr14102003@gmail.com>
2025-05-01 20:26:11 +0530

    sixth commit

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    // This will allow the build to succeed despite TypeScript errors
-    ignoreBuildErrors: true,
-  },
 };
 
 export default nextConfig;
